cbaed305dd6b2f83791d963a8202b657d714a6eb
Diana\ Moroz <diva14@yandex.by>
2022-11-25 14:13:50 +0300

    task01 str decision

--- task01.py
+++ task01.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+# TНапишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+number = input("Введите число: ")
+# с использованием строковых методов
+number_int = int(number.replace('.', ''))
+def sum_digit_for_int(num):
+    dig_sum = 0
+    while num > 0:
+        dig_sum += num % 10
+        num = num // 10
+    return dig_sum
+
+print(f'Сумма цифр числа {number} равна {sum_digit_for_int(number_int)}')
+
+#без использования строковых методов
+number_float = float(input("Введите вещественное число: "))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
